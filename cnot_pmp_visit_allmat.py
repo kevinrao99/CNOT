08bde5c7f0dec750e17b6accafbfd499b5fb97b9
Kevin Rao <krao@dhcp-10-250-101-12.harvard.edu>
2021-11-11 17:57:28 -0500

    runnable but incorrect for weird reasons

--- cnot_pmp_visit_allmat.py
+++ cnot_pmp_visit_allmat.py
@@ -1,13 +1,28 @@
 import collections
 import cnot_util
 
+def perm_as_pmp(perm, matrix):
+	# constructs a new matrix without modifying the original, outputs PMP^-1
+	global N
+
+	ans = []
+
+	# row i in answer is row perm[i] in matrix
+	for i in range(N):
+		ans.append([])
+		for j in range(N):
+			ans[i].append(matrix[perm[i]][perm[j]])
+
+	return ans
+
 def visit_equiv_matrices(matrix):
 	# Goes through all permutations, does PMP^-1, marks the result as visited.
 	global N
 	global distances
 	global reached
 
-	cur_distance = reached[cnot_util.matrix_to_int(matrix)]
+	cur_hash = cnot_util.matrix_to_int(matrix)
+	cur_distance = reached[cur_hash]
 
 	perm = range(N)
 
@@ -15,8 +30,24 @@ def visit_equiv_matrices(matrix):
 
 		# apply PMP^-1, hash result with matrix_to_int, store in reached and distances
 
-		perm = cnot_util.next_permutation(perm)
+		permuted_matrix = perm_as_pmp(perm, matrix)
+		if cur_distance == 1:
+			for arr in permuted_matrix:
+				print arr
+			print
 
+		permuted_hash = cnot_util.matrix_to_int(permuted_matrix)
+
+		if not (permuted_hash in reached):
+			reached[permuted_hash] = cur_distance
+
+			if not (cur_distance in distances):
+				print "shouldn't ever be here in visit_equiv_matrices"
+				distances[cur_distance] = []
+
+			distances[cur_distance].append(permuted_matrix)
+
+		perm = cnot_util.next_permutation(perm)
 
 	return None
 
@@ -93,7 +124,6 @@ def bfs():
 		if not (cur_op_length in distances):
 			distances[cur_op_length] = []
 
-		cur_matrix.sort()
 		distances[cur_op_length].append(cur_matrix)
 
 		visit_equiv_matrices(cur_matrix)
@@ -103,14 +133,33 @@ def bfs():
 if __name__ == "__main__":
 	global N
 	global M # total rows, extra are ancilla
+	global queue
 
 	N = 4
 	M = N
 
-	start_perm = range(5)
-	for i in range(120):
-		print start_perm
-		start_perm = cnot_util.next_permutation(start_perm)
+
+	queue = collections.deque([])
+
+	print "Running search of all matrices with PMP^-1 for N = " + str(N)
+
+	bfs()
+
+	dist_keys = distances.keys()
+	dist_keys.sort()
+
+	print
+	for key in dist_keys:
+		print str(key) + ": " + str(len(distances[key]))
+
+	distances[2] = sorted(distances[2])
+
+	'''
+	for matrix in distances[2]:
+		for i in range(len(matrix)):
+			print matrix[i]
+		print
+	'''
 
 
 
---
+++ cnot_visit_allmat.py
@@ -0,0 +1,126 @@
+import collections
+import cnot_util
+
+def extend(op_list):
+	global N
+	global queue
+	global reached
+
+	for i in range(1, N + 1):
+		for j in range(1, N + 1):
+			if i == j:
+				continue
+
+			if len(op_list) > 0:
+				(a, b) = op_list[len(op_list) - 1]
+
+				# if i, j, a, b are all distinct, order by i and a
+				# if i =/= a and j == b, order by i and a
+				# if i == a and j =/= b, order by j and b
+				# if i == a and j == b, do nothing
+
+				
+				if not (i == a) and not (i == b) and not (j == a):
+					if i > a:
+						continue
+				elif i == a:
+					if j >= b:
+						continue
+				'''
+
+				if i == a and j == b:
+					continue # Temporarily removing some optimization for testing
+				'''
+			to_append = op_list + [(i, j)]
+			to_append_hash = cnot_util.matrix_to_int(cnot_util.perform_ops(cnot_util.identity(N, N), to_append))
+			if to_append_hash in reached:
+				continue
+			queue.append(to_append)
+
+def bfs():
+	global queue
+	global N
+	global reached
+	global distances
+
+	reached = dict({}) # dict mapping a matrix hash to its distance to identity
+	distances = dict({}) # dict mapping a distance to the number (or list) of matrices that far from identity
+
+	queue.append([])
+
+	cur_op_length = 0
+
+	while len(queue) > 0:
+
+		cur = queue.popleft()
+
+		if len(cur) > cur_op_length:
+			print "\nstarting on " + str(len(cur)) + " operations",
+			cur_op_length = len(cur)
+
+			# Hacky fix to kill running
+			if distances[cur_op_length - 1] == 0:
+				break
+			# end hacky fix
+
+		cur_matrix = cnot_util.perform_ops(cnot_util.identity(N, N), cur)
+		cur_hash = cnot_util.matrix_to_int(cur_matrix)
+
+		if cur_hash in reached:
+			continue
+
+		reached[cur_hash] = cur_op_length
+		extend(cur)
+
+		if not (cur_op_length in distances):
+		#	distances[cur_op_length] = 0
+			distances[cur_op_length] = []
+
+		#if cnot_util.isUpperTriangular(cur_matrix):
+		#	distances[cur_op_length] = distances[cur_op_length] + 1
+		
+		distances[cur_op_length].append(cur_matrix)
+
+
+
+if __name__ == '__main__':
+	global queue
+	global target
+	global N
+	global distances
+	
+
+	N = 4
+	queue = collections.deque([])
+
+	print "Running visit allmat on N = " + str(N)
+
+	bfs()
+
+	dist_keys = distances.keys()
+	dist_keys.sort()
+
+	print
+	for key in dist_keys:
+		print str(key) + ": " + str(len(distances[key]))
+
+	'''
+	for key in dist_keys:
+		for mat in distances[key]:
+			for i in range(len(mat)):
+				for j in mat[i]:
+					print str(j) + " ",
+				print
+			print
+	
+	'''
+	distances[2] = sorted(distances[2])
+
+	for matrix in distances[2]:
+		for i in range(len(matrix)):
+			print matrix[i]
+		print
+
+
+
+	
