ffa4e9a0e983062a53f4fb024557f163df7d43fd
Kevin Rao <krao@dhcp-10-250-101-12.harvard.edu>
2021-11-12 14:16:43 -0500

    outputs number of classes at each distance

--- cnot_pmp_visit_allmat.py
+++ cnot_pmp_visit_allmat.py
@@ -1,6 +1,16 @@
 import collections
 import cnot_util
 
+class PMP_class:
+
+	def __init__(self, representative, distance):
+		self.rep = representative
+		self.dist = distance
+		self.members = []
+		self.adjacent = []
+
+
+
 def perm_as_pmp(perm, matrix):
 	# constructs a new matrix without modifying the original, outputs PMP^-1
 	global N
@@ -16,20 +26,19 @@ def perm_as_pmp(perm, matrix):
 	return ans
 
 def visit_equiv_matrices(matrix):
-	# Goes through all permutations, does PMP^-1, marks the result as visited.
+	# makes a pmp class
+	# Goes through all permutations, does PMP^-1, marks the result as visited and adds it to the pmp class
 	global N
 	global distances
 	global reached
+	global pmp_to_matrix
+	global matrix_to_pmp
 
 	cur_hash = cnot_util.matrix_to_int(matrix)
 	cur_distance = reached[cur_hash]
 
-	'''
-	print
-	for arr in matrix:
-		print arr
-	print
-	'''
+	cur_pmp = PMP_class(matrix, cur_distance)
+	pmp_to_matrix[cur_pmp] = matrix
 
 	perm = range(N)
 
@@ -39,6 +48,9 @@ def visit_equiv_matrices(matrix):
 
 		permuted_matrix = perm_as_pmp(perm, matrix)
 		permuted_hash = cnot_util.matrix_to_int(permuted_matrix)
+
+		cur_pmp.members.append(permuted_matrix)
+		matrix_to_pmp[permuted_hash] = cur_pmp
 
 		if not (permuted_hash in reached):
 			reached[permuted_hash] = cur_distance
@@ -58,6 +70,8 @@ def extend(op_list):
 	global M
 	global queue
 	global reached
+	global pmp_to_matrix
+	global matrix_to_pmp
 
 	for i in range(1, M + 1):
 		for j in range(1, M + 1):
@@ -88,8 +102,10 @@ def extend(op_list):
 
 			to_append = op_list + [(i, j)]
 			to_append_hash = cnot_util.matrix_to_int(cnot_util.perform_ops(cnot_util.identity(N, M), to_append))
+
 			if to_append_hash in reached:
 				continue
+
 			queue.append(to_append)
 
 def bfs():
@@ -99,8 +115,13 @@ def bfs():
 	global reached
 	global distances
 
+	global pmp_to_matrix
+	global matrix_to_pmp
+
 	reached = dict({}) # dict mapping a matrix hash to its distance to identity
 	distances = dict({}) # dict mapping a distance to the number (or list) of matrices that far from identity
+	pmp_to_matrix = dict({}) # maps a pmp class to its representative
+	matrix_to_pmp = dict({}) # maps a matrix hash to its pmp class
 
 	queue.append([])
 
@@ -117,7 +138,6 @@ def bfs():
 		cur_matrix = cnot_util.perform_ops(cnot_util.identity(N, M), cur) # for M total rows, extra are ancilla
 		cur_hash = cnot_util.matrix_to_int(cur_matrix)
 
-
 		if cur_hash in reached:
 			continue
 
@@ -130,6 +150,45 @@ def bfs():
 		distances[cur_op_length].append(cur_matrix)
 
 		visit_equiv_matrices(cur_matrix)
+
+def pmp_adjacent(pmp_1, pmp_2):
+	global N
+	global pmp_to_matrix
+	global matrix_to_pmp
+
+	class_rep = pmp_1.rep
+
+	for i in range(N + 1):
+		for j in range(N + 1):
+			if i == j:
+				continue
+
+			cnot_util.operation(class_rep, i, j)
+			if class_rep in pmp_2.members:
+				cnot_util.operation(class_rep, i, j)
+				return True
+			else:
+				cnot_util.operation(class_rep, i, j)
+
+	return False
+
+
+def build_pmp_graph():
+	global N
+	global pmp_to_matrix
+	global matrix_to_pmp
+
+	classes = pmp_to_matrix.keys()
+
+	for pmp_class in classes:
+		for other_class in classes:
+			if pmp_class == other_class:
+				continue
+
+			if pmp_adjacent(pmp_class, other_class):
+				pmp_class.adjacent.append(other_class)
+
+
 
 
 
@@ -152,6 +211,7 @@ if __name__ == "__main__":
 	dist_keys.sort()
 
 	print
+	print "Matrix distances:"
 	for key in dist_keys:
 		print str(key) + ": " + str(len(distances[key]))
 
@@ -163,6 +223,20 @@ if __name__ == "__main__":
 			print matrix[i]
 		print
 	'''
+
+	print
+	pmp_keys = pmp_to_matrix.keys()
+	dist_ct = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+	for key in pmp_keys:
+		dist_ct[key.dist] += 1
+
+	print "equiv classes distances:"
+	for i in range(len(dist_ct)):
+		if dist_ct[i] == 0:
+			continue
+		print str(i) + ": " + str(dist_ct[i])
+
 
 
 
